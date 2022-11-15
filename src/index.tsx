6d30ce205f629936cd9f366375a58daa04a95b2b
Yuri Rodrigues Teixeira <71528635+yuri3gp@users.noreply.github.com>
2022-11-15 17:14:18 -0300

    fix

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { RouterProvider } from 'react-router-dom';
-import App from './App';
 import router from './routes';
 
 ReactDOM.render(
